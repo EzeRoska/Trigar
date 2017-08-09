69c4e1dc56da0d8fdaf3ecdb0591dd1dadf8005c
EzeRoska <roska47@gmail.com>
2017-08-09 14:27:25 -0300

    miercoles 9

--- app/src/main/java/com/example/claudiopc/trigar/MovimientoFragment.java
+++ app/src/main/java/com/example/claudiopc/trigar/MovimientoFragment.java
@@ -44,6 +44,7 @@ public class MovimientoFragment extends Fragment implements View.OnClickListener
     CalendarView calendari;
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View Vista=inflater.inflate(R.layout.fragment_movimiento,container,false);
