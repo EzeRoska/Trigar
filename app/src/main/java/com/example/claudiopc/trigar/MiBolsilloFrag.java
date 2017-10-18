d3a3e49b83520424b01bb405fabf28faf59aaaff
Ezequiel <roska47@gmail.com>
2017-10-17 21:46:01 -0300

    hola
    Modifique cantidad en mibolsillo

--- app/src/main/java/com/example/claudiopc/trigar/MiBolsilloFrag.java
+++ app/src/main/java/com/example/claudiopc/trigar/MiBolsilloFrag.java
@@ -1,6 +1,9 @@
 package com.example.claudiopc.trigar;
 
+        import android.app.Activity;
         import android.content.Context;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
         import android.net.Uri;
         import android.os.Bundle;
         import android.support.v4.app.Fragment;
@@ -12,6 +15,9 @@ package com.example.claudiopc.trigar;
 
 public class MiBolsilloFrag extends Fragment {
     RssHandler a;
+    Basededatos accesobase;
+    SQLiteDatabase Base;
+
     MovimientoFragment b;
     int convertircotizaciontrigo;
     int convertircotizacionMaiz;
@@ -27,9 +33,50 @@ public class MiBolsilloFrag extends Fragment {
     int MultiplicarCebada;
     int Cantidad;//PREGUNTAR
     Spinner txtGrano;//preguntar
+    String cantidadtrigo;
+    String cantidadmaiz;
+    String cantidadsoja;
+    String cantidadcebada;
+    String Nombre;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String text = b.txtGrano.getSelectedItem().toString();
+        if(baseDeDatosAbierta())
+        {
+            Cursor conjuntoderegistros;
+            conjuntoderegistros=Base.rawQuery("select Cantidad From Movimiento",null);
+            if(conjuntoderegistros.moveToFirst()==true)
+            {
+                int cantidadderegistros=0;
+                do {
+                    cantidadderegistros++;
+                    Nombre=conjuntoderegistros.getString(0);
+                }
+                while (conjuntoderegistros.moveToNext()==true);
+                if(text=="trigo")
+                {
+                    cantidadtrigo=Nombre;
+                }
+                if (text=="Maiz")
+                {
+                    cantidadmaiz=Nombre;
+                }
+                if(text=="Soja")
+                {
+                    cantidadsoja=Nombre;
+
+                }
+                if(text=="cebada")
+                {
+                    cantidadcebada=Nombre;
+
+                }
+
+
+            }
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mi_bolsillo, container, false);
     }
@@ -38,6 +85,19 @@ public class MiBolsilloFrag extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    private boolean baseDeDatosAbierta() {
+        boolean responder;
+        accesobase=new Basededatos(this,"baseDeDatos",null,1);
+        Base=accesobase.getWritableDatabase();
+        if (Base != null) {
+            responder = true;
+        }
+        else {
+            responder = false;
+        }
+        return responder;
+
     }
     public void mult()
     { cotizaciontrigo=a.cotizTrigo;
